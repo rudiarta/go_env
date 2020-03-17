7865ffee938a139c1422b48da968b16093ddf9d9
rudirahardian <rudi.rahardian@ralali.com>
2020-03-17 19:45:46 +0700

    modify patern

--- main.go
+++ main.go
@@ -1,25 +1,9 @@
 package main
 
 import (
-	"net/http"
 	"github.com/rudirahardian/go_env/app/routes"
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	// gin.SetMode(gin.ReleaseMode)
-	gin.SetMode(gin.DebugMode)
-	router := gin.Default()
-
-	v1 := router.Group("/api/v1/todos")
-	{
-		v1.GET("/", createTodo)
-	}
-	router.Run()
-
-}
-
-// createTodo add a new todo
-func createTodo(c *gin.Context) {
-	c.JSON(http.StatusCreated, gin.H{"message": "Todo item created successfully!", routes.Test: 1})
+	routes.RouteInit()
 }
